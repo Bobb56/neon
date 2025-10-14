f8fa79a0e4e9c363780942609cc5888d3da53a94
= <lepuillandre.raphael@gmail.com>
2025-10-14 22:06:00 +0200

    Réglé le pb du floodfill

--- src/headers/lowlevel.h
+++ src/headers/lowlevel.h
@@ -1,6 +1,7 @@
 #ifndef LOWLEVEL_H
 #define LOWLEVEL_H
 
+#include "constants.h"
 #include "runtime.h"
 #include "objects.h"
 #include "processcycle.h"
@@ -9,5 +10,9 @@ void reset_stack_and_registers(Process* process);
 void save_stack_and_registers(Process* process);
 void switch_registers(Process* process, Process* prev);
 void* get_stack(void);
+
+#ifdef TI_EZ80
+void* floodfill_switch_stack(void* new_stack);
+#endif
 
 #endif
