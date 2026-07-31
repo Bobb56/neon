477130254de8931d1b8f8b6b064ee6de190ab8e3
Raphaël Le Puillandre <rlpuillandre@Debian>
2026-07-31 17:07:57 +0200

    Complete 4.5 update and add animated png demo

--- deploy/deploy.3.sh
+++ deploy/deploy.3.sh
@@ -11,7 +11,9 @@ echo "Adding new version to download page..."
 python3 publish.py $1
 python3 downloads.py
 
-cd $ROOT/deploy
+cd $ROOT
+zip -r $RELEASE_FOLDER.zip $RELEASE_FOLDER/*
+mv $RELEASE_FOLDER.zip ../distribution/
 rm -r $RELEASE_FOLDER
 
 echo "Neon website is ready for next update. Don't forget to git push the website for final step!"
